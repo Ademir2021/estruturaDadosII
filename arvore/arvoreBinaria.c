73adaff0bd87fbce1b57b6ba75ea46d81f1254b1
Ademir Souza de Almeida <centroserra@gmail.com>
2022-05-30 17:33:03 -0300

    arvores binarias

--- arvore/arvoreBinaria.c
+++ arvore/arvoreBinaria.c
@@ -9,23 +9,32 @@
 
 //Programa 1.1 - Estrutura no nó
 struct str_no{
-    char dado;
+    int dado;
     int esquerda;
     int direita;
     int pai;
-}str_no;
+};
 
 //Programa 1.2 - Variaveis
 struct str_no arvore[tamanho];
 int lado, indice=0;
 int opt=-1;
-char pai, no;
+int pai, no;
 
 //Prototipação
-void arvore_insere(int pai, char dado, int lado);
-int arvore_procura(char dado);
+void arvore_insere(int pai, int dado, int lado);
+int arvore_procura(int dado);
 void menu_mostrar(void);
-void preOrdem(struct str_no* arvore); //Percurso Pré-Ordem
+//void preOrdem(void); //Percurso Pré-Ordem
+
+
+void preOrdem(struct str_no* raiz){
+    if (raiz != 0){
+        printf("%i \t", raiz->dado); //visita o nó raiz
+        preOrdem(raiz->direita);
+        // preOrdem(raiz->direita);
+    }
+}
 
 /* Função Principal */
 int main(void){
@@ -37,11 +46,11 @@ do {
     switch (opt){
         case 1:
         printf("\nDigite o valor do PAI: ");
-        scanf("%c", &pai);
-        scanf("%c", &pai);
+        scanf("%d", &pai);
+        //scanf("%c", &pai);
         printf("Digite o valor do NO: ");
-        scanf("%c", &no);
-        scanf("%c", &no);
+        scanf("%d", &no);
+        //scanf("%c", &no);
         printf("Digite o lado da subarvore (E=%d/D=%d/R=%d): ", E,D,R);
         scanf("%d", &lado);
         temp = arvore_procura(pai);
@@ -49,10 +58,10 @@ do {
         break;
         case 2:
         printf("Digite o valor do NO: ");
-        scanf("%c", &no);
-        scanf("%c", &no);
+        scanf("%d", &no);
+        //scanf("%d", &no);
         temp = arvore_procura(no);
-        printf("No %c\nFilho Esquerda: %c\nFilho Direita: %c\n\n",
+        printf("No %d\nFilho Esquerda: %d\nFilho Direita: %d\n\n",
                 arvore[temp].dado,
                 arvore[arvore[temp].esquerda].dado,
                 arvore[arvore[temp].direita].dado);
@@ -60,17 +69,20 @@ do {
         getchar();
         break;
         case 3:
+        printf("\nPercurso preOrdem\n\n");
         preOrdem(arvore);
+        getchar();
+        getchar();
         break;
             }
         }while (opt!=0);
         getchar();
-        getchar();
+        //getchar();
         return(0);
 }
 
 //Programa 1.4 - Inserir nó
-void arvore_insere(int pai, char dado, int lado){
+void arvore_insere(int pai, int dado, int lado){
     switch(lado){
         case E:
         arvore[pai].esquerda = indice;
@@ -99,7 +111,7 @@ void arvore_insere(int pai, char dado, int lado){
 }
 
 //Programa 1.3 - Procura nó
-int arvore_procura(char dado){
+int arvore_procura(int dado){
     if(indice !=0){
         for(int i = 0; i<indice; i++){
             if(arvore[i].dado == dado){
@@ -116,20 +128,14 @@ int arvore_procura(char dado){
 void menu_mostrar(void){
     system("clear");
     for(int i = 0; i < indice; i++){
-        printf("| %c", arvore[i].dado);
+        printf("| %d", arvore[i].dado);
     }
     printf("\n1 - Inserir um NO na árvore");
     printf("\n2 - Pesquisar um NO na árvore");
-    printf("\n3 - Percurso Pré-Ordem");
+    printf("\n3 - Mostrar Percurso Pré-Ordem");
     printf("\n0 - Sair...");
 }
 
-//Percurso Pré-Ordem
-void preOrdem(struct str_no* arvore){
-    if(arvore){
-        printf("%d \t", arvore->dado); //Visita o nó atual
-        preOrdem(arvore->esquerda);
-        preOrdem(arvore->direita);
-    }
-    retrun();
-}
+//Teste para imprimir PŕeOrdem
+
+//
---
+++ arvore/arvoreTeste.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct no{
+    int valor;
+    struct no *direita, *esquerda;
+}NoArv;
+
+NoArv* inserir(NoArv *raiz, int num){
+    if(raiz == NULL){
+        NoArv *aux = malloc(sizeof(NoArv));
+        aux->valor = num;
+        aux->esquerda = NULL;
+        aux->direita = NULL;
+        return aux;
+    }
+    else{
+        if(num < raiz->valor)
+            raiz->esquerda = inserir(raiz->esquerda, num);
+        else
+            raiz->direita = inserir(raiz->direita, num);
+        return raiz;
+    }
+}
+
+void menuOpcoes(){
+    printf("Menu de Opções\n");
+    printf("1 - Inserir Valor\n");
+    printf("2 - Imprimir Pré-Ordem\n");
+    printf("0 - Sair\n");
+}
+
+
+
+void preOrdem(NoArv *raiz){ // 50 25 30 100
+    if(raiz){
+        printf("%d ", raiz->valor);
+        preOrdem(raiz->esquerda);
+        preOrdem(raiz->direita);
+    }
+}
+
+
+int main(){
+
+    NoArv *raiz = NULL;
+    int opcao, valor;
+
+    do{
+        menuOpcoes();
+        scanf("%d", &opcao);
+        
+    switch(opcao){
+        case 1:
+            printf("\n\tDigite um valor: ");
+            scanf("%d", &valor);
+            system("clear");
+            raiz = inserir(raiz, valor);
+            break;
+        case 2:
+            printf("\n\tImpressao Pré-Ordem:\n\t");
+            preOrdem(raiz);
+            printf("\n");
+            break;
+        default:
+            if(opcao != 0);
+                printf("\n\tOpcao invalida!!!\n");
+        }
+
+    }while(opcao != 0);
+
+    return 0;
+}
